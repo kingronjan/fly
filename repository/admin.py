81412e0033cd3818c31856d1bf3442630d8ff245
Hifinancial <financial@github.com>
2019-08-29 22:34:34 +0800

    update: register models to admin site

--- repository/admin.py
+++ repository/admin.py
@@ -3,5 +3,12 @@ from repository import models
 
 # Register your models here.
 
-admin.register(models.User)
-admin.register(models.Blog)
+admin.site.register(models.User)
+admin.site.register(models.Blog)
+admin.site.register(models.Theme)
+admin.site.register(models.Repoting)
+admin.site.register(models.Classification)
+admin.site.register(models.Tag)
+admin.site.register(models.Article)
+admin.site.register(models.LikeToArticle)
+admin.site.register(models.Comment)
