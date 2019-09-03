21211a4d2bb2f1804ee98de99592ea044aa25e27
financial <financialfly@outlook.com>
2019-09-03 17:47:58 +0800

    add: join & upload image

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('join/', views.join, name='join'),
+    path('upload_image/', views.upload_image, name='upload_image'),
 ]
