b536fa0843344a5480615f5b706b6f4dbd7632a3
financial <financialfly@outlook.com>
2019-09-04 17:40:53 +0800

    add: user_index

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('join/', views.join, name='join'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signin/checkusername/', views.checkusername, name='checkusername'),
+    path('signout/', views.signout, name='signout'),
     path('upload_image/', views.upload_image, name='upload_image'),
-    path('captcha/', views.captcha, name='captcha'),
+    path('captcha/', views.handle_captcha, name='captcha'),
+    path('<str:username>', views.index, name='user_index'),
 ]
